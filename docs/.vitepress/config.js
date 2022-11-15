62f05f23da256265d4ce4c6487827d4585f90cc4
Igor Trindade <igortrindademe@gmail.com>
2022-11-15 07:20:06 -0300

    test: Testing base config for vitepress

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-  base: "/igortrindade/lazyfy",
+  base: "/lazyfy/",
   title: '@igortrindade/lazyfy',
   description: "Someone once said: \"I choose a lazy person to do a hard job. Because a lazy person will find an easy way to do it.\""
 })
