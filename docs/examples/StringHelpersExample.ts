fd9ab962422803eac3902717aa908834c636ca60
Igor Trindade <igortrindademe@gmail.com>
2022-11-15 16:14:06 -0300

    chore: Minor hint on example

--- docs/examples/StringHelpersExample.ts
+++ docs/examples/StringHelpersExample.ts
@@ -2,6 +2,7 @@
 export const randomString = {
   title: 'Generate random string',
   code: `
+// The result here keeps changing because how code mirror dispatch their updates
 result = StringHelpers.randomString(32)
   `
 }
