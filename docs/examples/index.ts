d17e92a2493a76d3281b6ff3ce56c3e59315b26c
Igor Trindade <igortrindademe@gmail.com>
2022-11-15 15:32:33 -0300

    fix: Testing fix auto map module

--- docs/examples/index.ts
+++ docs/examples/index.ts
@@ -1,4 +1,4 @@
 import * as ArrayFind from './ArrayFind'
 import * as StringHelpers from './StringHelpers'
 
-export default [ ...Object.values(ArrayFind), ...Object.values(StringHelpers) ]
+export default [ ...Object.keys(ArrayFind).map((key) => ArrayFind[key]), ...Object.keys(StringHelpers).map((key) => StringHelpers[key]) ]
