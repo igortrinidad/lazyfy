cfbc762d4aef58468b95f3e734d1f76e2e1e27e2
Igor Trindade <igortrindademe@gmail.com>
2022-11-16 07:29:56 -0300

    test: Added case tests in removeAll

--- tests/array_helpers_removeAll.test.ts
+++ tests/array_helpers_removeAll.test.ts
@@ -3,24 +3,30 @@ import { books, fruits } from './helpers/example_arrays'
 
 test('Remove all items that match based obj query', () => {
   expect(ArrayHelpers.removeAll(books, { category: 'programming' }).length).toBe(1)
+  expect(books.length).toBe(3)
 })
 
 test('Remove all items that match based obj query', () => {
   expect(ArrayHelpers.removeAll(books, { category: ['programming', 'self help'] }).length).toBe(0)
+  expect(books.length).toBe(3)
 })
 
 test('Remove all items that match based obj query', () => {
   expect(ArrayHelpers.removeAll(books, { category: ['self help'] }).length).toBe(2)
+  expect(books.length).toBe(3)
 })
 
 test('Remove all items that match based string query', () => {
   expect(ArrayHelpers.removeAll(fruits, 'watermelon').length).toBe(2)
+  expect(fruits.length).toBe(3)
 })
 
 test('Remove all items that match based array string query', () => {
   expect(ArrayHelpers.removeAll(fruits, ['watermelon', 'pineapple']).length).toBe(1)
+  expect(fruits.length).toBe(3)
 })
 
 test('Remove all items that match based array string query', () => {
   expect(ArrayHelpers.removeAll(fruits, ['lemon']).length).toBe(3)
+  expect(fruits.length).toBe(3)
 })
