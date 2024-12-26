ca6bd0622120e948c76e1054a294d3633f9f0097
Igor Trindade <igortrindademe@gmail.com>
2024-12-26 17:26:31 -0300

    test: Enhance Masker tests for various phone number formats

--- tests/Masker/masker.test.ts
+++ tests/Masker/masker.test.ts
@@ -4,6 +4,12 @@ test('Mask multiple phone numbers', () => {
   expect(Masker.mask(31990909090, Masker.DEFAULT_PHONE_MASK)).toEqual('(31) 99090-9090')
   expect(Masker.mask('31990909090', Masker.DEFAULT_PHONE_MASK)).toEqual('(31) 99090-9090')
   expect(Masker.mask('5531990909090', Masker.DEFAULT_PHONE_MASK_WITH_DDI)).toEqual('+55 (31) 99090-9090')
+  expect(Masker.mask('+55 (31) 99090-9090', Masker.DEFAULT_PHONE_MASK_WITH_DDI)).toEqual('+55 (31) 99090-9090')
+  expect(Masker.mask('+55(31) 99090-9090', Masker.DEFAULT_PHONE_MASK_WITH_DDI)).toEqual('+55 (31) 99090-9090')
+  expect(Masker.mask('+55(31)99090-9090', Masker.DEFAULT_PHONE_MASK_WITH_DDI)).toEqual('+55 (31) 99090-9090')
+  expect(Masker.mask('+55(3199090-9090', Masker.DEFAULT_PHONE_MASK_WITH_DDI)).toEqual('+55 (31) 99090-9090')
+  expect(Masker.mask('+553199090-9090', Masker.DEFAULT_PHONE_MASK_WITH_DDI)).toEqual('+55 (31) 99090-9090')
+  expect(Masker.mask('553199090-9090', Masker.DEFAULT_PHONE_MASK_WITH_DDI)).toEqual('+55 (31) 99090-9090')
 })
 
 test('Unmask multiple phone numbers', () => {
