ff62552a149a04b47ae357e840a74de998667e28
Igor Trindade <igortrindademe@gmail.com>
2025-02-07 02:19:42 -0300

    test(Masker): add test for masking Whatsapp Remote JID

--- tests/Masker/masker.test.ts
+++ tests/Masker/masker.test.ts
@@ -17,3 +17,7 @@ test('Unmask multiple phone numbers', () => {
   expect(Masker.unmask('(31) 99090-9090', Masker.DEFAULT_PHONE_MASK)).toEqual('31990909090')
   expect(Masker.unmask('+55 (31) 99090-9090', Masker.DEFAULT_PHONE_MASK_WITH_DDI)).toEqual('5531990909090')
 })
+
+test('Mask Whatsapp Remote JID', () => {
+  expect(Masker.mask('553112341234@s.whatsapp.net', Masker.DEFAULT_PHONE_MASK_WITH_DDI)).toEqual('+55 (31) 1234-1234')
+})
