8e9a7d2144bed1cdf2fdf4aafa5a8e508c091bc9
Igor Trindade <igortrindademe@gmail.com>
2026-04-27 14:18:10 -0300

    feat: add support for extracting UUIDs version 7
    - Added `extractUuidsV7` function to extract UUIDs version 7 from a given text using a specific regex pattern.
    - Updated TypeScript definitions in both `RegexHelpers.d.ts` files to include the new function.
    - Modified the implementation files `RegexHelpers.js` to export the new function and include it in the `RegexHelpers` object.

--- dist/browser/RegexHelpers.d.ts
+++ dist/browser/RegexHelpers.d.ts
@@ -1,6 +1,8 @@
 export declare const extractMatchs: (text: string, regex: RegExp) => Array<string>;
 export declare const extractUuidsV4: (text: string) => Array<string>;
+export declare const extractUuidsV7: (text: string) => Array<string>;
 export declare const RegexHelpers: {
     extractMatchs: (text: string, regex: RegExp) => Array<string>;
     extractUuidsV4: (text: string) => Array<string>;
+    extractUuidsV7: (text: string) => Array<string>;
 };
--- dist/main/RegexHelpers.js
+++ dist/main/RegexHelpers.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.RegexHelpers = exports.extractUuidsV4 = exports.extractMatchs = void 0;
+exports.RegexHelpers = exports.extractUuidsV7 = exports.extractUuidsV4 = exports.extractMatchs = void 0;
 const extractMatchs = (text, regex) => {
     const matches = text.match(regex) || [];
     return [...new Set(matches)];
@@ -11,7 +11,13 @@ const extractUuidsV4 = (text) => {
     return (0, exports.extractMatchs)(text, regex);
 };
 exports.extractUuidsV4 = extractUuidsV4;
+const extractUuidsV7 = (text) => {
+    const regex = /[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-7[a-fA-F0-9]{3}-[89abAB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}/g;
+    return (0, exports.extractMatchs)(text, regex);
+};
+exports.extractUuidsV7 = extractUuidsV7;
 exports.RegexHelpers = {
     extractMatchs: exports.extractMatchs,
-    extractUuidsV4: exports.extractUuidsV4
+    extractUuidsV4: exports.extractUuidsV4,
+    extractUuidsV7: exports.extractUuidsV7
 };
